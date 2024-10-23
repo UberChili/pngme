c567b284428961b7f7da6f28ae7114cd71cdb4b4
Juan Andres Nuñez <nunezlopez.ja@gmail.com>
2024-10-22 18:03:52 -0600

    Added initial idea for command-line arguments. Not sure how to follow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use clap::Parser;
+
 mod args;
 mod chunk;
 mod chunk_type;
@@ -8,5 +10,14 @@ pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
 
 fn main() -> Result<()> {
-    todo!()
+    let args = commands::Args::parse();
+
+    println!("{:?}", args);
+
+    let filepath = args.filepath;
+    let chunk_type = args.chunk_type;
+    let message = args.message;
+    let output_file = args.out_file;
+
+    Ok(())
 }
